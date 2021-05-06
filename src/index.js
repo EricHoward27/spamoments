e7c208a84f6a3be6b9947d633483351bc6f05a55
eric <erichoward2141@gmail.com>
2021-05-06 18:19:56 -0500

    version 1.1 -add styling to layout components -adjusted masthead bg and color to match theme pallete -modify nav brand name and footer -next iteration will included nav brand logo and some more color pallete changes to layout components

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '../src/assets/css/styles.css'
+import '../src/assets/css/styles.css'
 
 ReactDOM.render(
   <React.StrictMode>
