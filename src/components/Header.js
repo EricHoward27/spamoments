52a7908b755e0e2eabc9acfbe99a78824735140e
eric <erichoward2141@gmail.com>
2021-05-08 18:36:14 -0500

    add logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
         <div className="container">
-            <a className="navbar-brand js-scroll-trigger" href="#">Spa Moments by Serena</a>
+            <a className="navbar-brand js-scroll-trigger" href="#"><img src="spamoment.png" alt="Spa Moment Logo" width="90" height="90"/>Spa Moments by Serena</a>
             <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
             <div className="collapse navbar-collapse" id="navbarResponsive">
                 <ul className="navbar-nav ml-auto my-2 my-lg-0">
